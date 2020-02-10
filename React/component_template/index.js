bc47a4f581dc20787a229c66150b931503f54ead
Karl Chvojka <karl.chvojka@gmail.com>
2020-02-10 10:24:15 -0500

    Added component template

---
+++ React/component_template/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+// TODO: CHANGE THIS NAME
+function Your_Component() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+// TODO: CHANGE IT HERE TOO
+export default Your_Component;
